5b04e24ed47d1e2394ec208de533378ad73d55ed
Andy Nguyen <47223678+anguye13@users.noreply.github.com>
2020-07-25 17:54:47 -0400

    Add files via upload

---
+++ food-bot/FoodBot.py
@@ -0,0 +1,46 @@
+import praw
+import tweepy
+import time
+
+# Timer constant - 6H
+INTERVAL = 60 * 60 * 6
+
+# Fill in keys
+Reddit_ClientID =
+Reddit_SecretID =
+Reddit_Agent =
+
+Twitter_APIKey =
+Twitter_APISecret =
+Twitter_AccessToken =
+Twitter_AccessSecret =
+
+# Bot that takes the top daily submission from r/food and tweets it
+def FoodBot():
+    # Authenticate to reddit
+    reddit = praw.Reddit(client_id = Reddit_ClientID,
+                         client_secret = Reddit_SecretID,
+                         user_agent = Reddit_Agent)
+
+    # Authenticate to twitter
+    auth = tweepy.OAuthHandler(Twitter_APIKey,
+                               Twitter_APISecret)
+
+    auth.set_access_token(Twitter_AccessToken,
+                          Twitter_AccessSecret)
+
+    twitter = tweepy.API(auth)
+
+    while True:
+        # List generator of r/food filtered by top daily
+        top_submissions = reddit.subreddit('food').top(time_filter='day', limit=4)
+        
+        # Go through top submissions tweeting 1 every 6H
+        try:
+            for submission in top_submissions:
+                twitter.update_status("https://reddit.com" + submission.permalink)
+                time.sleep(INTERVAL)
+        except tweepy.TweepError:
+            pass
+        
+FoodBot()
